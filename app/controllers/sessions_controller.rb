feb3b46f39cdce3dfae40ad1be32969691a4c1d4
mehrad <hossein.ataeefard@gmail.com>
2019-09-13 00:04:33 +0430

    code clean up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,21 @@
 class SessionsController < ApplicationController
- def new
- end
+  def new; end
 
- def create
-  user = User.find_by(email: params[:email])
-  if user && user.authenticate(params[:password])
-   session[:user_id] = user.id
-   flash[:success] = ‘Successfully Logged In!’
-   redirect_to ‘/contacts’
-  else
-   flash[:warning] = “Invalid Username or Password”
-   redirect_to ‘/login’
+  def create
+    user = User.find_by(email: params[:email])
+    if user&.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Successfully Logged In!'
+      redirect_to '/'
+    else
+      flash[:warning] = 'Invalid'
+      redirect_to '/login'
+    end
   end
- end
- def destroy
-  session[:user_id] = nil
-  flash[:success] = “Successfully Logged Out!”
-  redirect_to ‘/login’
- end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Successfully Logged Out!'
+    redirect_to '/login'
+  end
 end
