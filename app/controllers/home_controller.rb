b8e3f25e78ea12989f813b88dd850380cd415b3b
mehrad <hossein.ataeefard@gmail.com>
2019-09-12 23:59:21 +0430

    redirect if not login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    if current_user
+      @name = current_user.name
+      render 'index.html.erb'
+    else
+      flash[:warning] = 'Login please'
+      redirect_to '/login'
+    end
   end
 end
